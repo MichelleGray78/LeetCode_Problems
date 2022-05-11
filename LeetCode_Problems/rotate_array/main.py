da03d6d7459447a9fa96543a63abe2108ab659e8
Michelle <56207268+MichelleGray78@users.noreply.github.com>
2022-05-11 10:46:07 +0100

    better_solution_rotate_array

--- LeetCode_Problems/rotate_array/main.py
+++ LeetCode_Problems/rotate_array/main.py
@@ -1,16 +1,5 @@
 def rotate(nums, k):
-    n = len(nums)
-    k %= n
-    
-    start = count = 0
-    while count < n:
-        current, prev = start, nums[start]
-        while True:
-            next_idx = (current + k) % n
-            nums[next_idx], prev = prev, nums[next_idx]
-            current = next_idx
-            count += 1
-            
-            if start == current:
-                break
-        start += 1
+    for i in range(k):
+        nums.insert(0, nums[-1])
+        del nums[-1]
+        print(nums)
